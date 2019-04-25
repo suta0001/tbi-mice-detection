90634ed7689588f5c7f5d73e94fc150c3d136fce
Agustinus Sutandi <sutandia@tigger-mbp1.local>
2019-04-24 21:01:13 -0700

    Added command-line parameters to select eeg source and number of classes. Scaled eeg epoch to mV range (divide by 1000). Normalize the zero-crossing rate.

--- createfeaturestimed.py
+++ createfeaturestimed.py
@@ -11,12 +11,13 @@ import sys
 
 
 # parameter to be varied
-eeg_epoch_width_in_s = int(sys.argv[1])
-eeg_source = 'pp2'
+eeg_epoch_width_in_s = int(sys.argv[2])
+eeg_source = sys.argv[1]
+num_classes = int(sys.argv[3])
 
 # set up file location paths
-epochs_path = 'data/epochs/'
-timed_path = 'data/timed/'
+epochs_path = 'data/epochs_{0}c/'.format(str(num_classes))
+timed_path = 'data/timed_{0}c/'.format(str(num_classes))
 
 epochs_files = [file for file in os.listdir(epochs_path) if
                 '_{0}_ew{1}'.format(eeg_source, eeg_epoch_width_in_s) in file]
@@ -29,14 +30,15 @@ for epochs_file in epochs_files:
     with open(epochs_filename, mode='r', newline='') as csvfile:
         filereader = csv.reader(csvfile)
         for row in filereader:
-            eeg_epoch = [float(i) for i in row]
+            eeg_epoch = [float(i) / 1000.0 for i in row]
             # extract features
             feature = []
             feature.append(statistics.mean(eeg_epoch))
             feature.append(statistics.variance(eeg_epoch))
             feature.append(skew(eeg_epoch))
             feature.append(kurtosis(eeg_epoch))
-            feature.append(ta.zero_crossing_rate(eeg_epoch))
+            feature.append(ta.zero_crossing_rate(eeg_epoch) /
+                           (len(eeg_epoch) - 1))
             feature.extend(ta.calc_hjorth_params(eeg_epoch))
             feature.append(np.percentile(eeg_epoch, 75))
             eeg_epochs.append(feature)
