8bc3c033dac57de8f40f1b0eebb3498b70d2f866
Agustinus Sutandi <sutandia@tigger-mbp1.local>
2019-04-24 22:44:22 -0700

    Copied from rf_sleep.py and updated the epoch selection to select wake epochs.

--- rf_wake.py
+++ rf_wake.py
@@ -7,26 +7,51 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.validation import column_or_1d
 import sourcedata as sd
+import statistics
 import sys
 
 
 # parameters to be varied
-eeg_epoch_width_in_s = int(sys.argv[1])
+eeg_epoch_width_in_s = int(sys.argv[2])
 pe_orders = list(range(3, 8))
 pe_delays = list(range(1, 11))
 max_degree = 11
-eeg_source = 'pp2'
+eeg_source = sys.argv[1]
+feature_type = sys.argv[3]
+target_names = ['Sham', 'TBI']
 
 # read dataset per fold
+accuracies = []
+reports = []
+template = None
+common_labels = None
+
 for fold in range(10):
-    cv_path = 'data/cv/'
-    template = '{0}_{1}_ew{2}_f{3}_{4}t{5}_{6}t{7}_{8}_sp_ti.csv'
-    common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold),
-                     str(pe_orders[0]), str(pe_orders[-1]),
-                     str(pe_delays[0]), str(pe_delays[-1]),
-                     str(max_degree)]
-    # template = '{0}_{1}_ew{2}_f{3}_sxx.csv'
-    # common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold)]
+    common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold)]
+    cv_path = 'data/cv_4c/'
+    if feature_type == 'pe':
+        template = '{0}_{1}_ew{2}_f{3}_{4}t{5}_{6}t{7}.csv'
+        common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold),
+                         str(pe_orders[0]), str(pe_orders[-1]),
+                         str(pe_delays[0]), str(pe_delays[-1])]
+    elif feature_type == 'vg':
+        template = '{0}_{1}_ew{2}_f{3}_{4}.csv'
+        common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold),
+                         str(max_degree)]
+    elif feature_type == 'sp':
+        template = '{0}_{1}_ew{2}_f{3}_sp.csv'
+    elif feature_type == 'ti':
+        template = '{0}_{1}_ew{2}_f{3}_ti.csv'
+    elif feature_type == 'sxx':
+        template = '{0}_{1}_ew{2}_f{3}_sxx.csv'
+    elif feature_type == 'wpe':
+        template = '{0}_{1}_ew{2}_f{3}_wpe.csv'
+    elif feature_type == 'all':
+        template = '{0}_{1}_ew{2}_f{3}_{4}t{5}_{6}t{7}_{8}_sp_ti.csv'
+        common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold),
+                         str(pe_orders[0]), str(pe_orders[-1]),
+                         str(pe_delays[0]), str(pe_delays[-1]),
+                         str(max_degree)]
     train_epochs = []
     train_labels = []
     test_epochs = []
@@ -75,12 +100,28 @@ for fold in range(10):
 
     # calculate accuracy score
     accuracy = accuracy_score(test_labels, predict_labels)
+    accuracies.append(accuracy)
     print('Fold = ' + str(fold) + ' Accuracy = ' + str(accuracy))
 
     # calculate confusion matrix
     print(confusion_matrix(test_labels, predict_labels))
 
     # print report
-    target_names = ['SW', 'TW']
+    report = classification_report(test_labels, predict_labels,
+                                   target_names=target_names,
+                                   output_dict=True)
+    reports.append(report)
     print(classification_report(test_labels, predict_labels,
                                 target_names=target_names))
+
+# print out results summary
+print('Mean  accuracy = ' + str(statistics.mean(accuracies)))
+print('Stdev accuracy = ' + str(statistics.stdev(accuracies)))
+for name in target_names:
+    precisions = [reports[i][name]['precision'] for i in range(len(reports))]
+    print('Mean  prec ' + name + '  = ' + str(statistics.mean(precisions)))
+    print('Stdev prec ' + name + '  = ' + str(statistics.stdev(precisions)))
+for name in target_names:
+    recalls = [reports[i][name]['recall'] for i in range(len(reports))]
+    print('Mean  recll ' + name + ' = ' + str(statistics.mean(recalls)))
+    print('Stdev recll ' + name + ' = ' + str(statistics.stdev(recalls)))
