773df7ce64864a72730a280b32d480ff47207169
Agustinus Sutandi <sutandia@tigger-mbp1.local>
2019-05-08 22:05:42 -0700

    Modified to write one data-label pair per file.  This is needed to shuffle data in the data generator.

--- createcvfoldsraw.py
+++ createcvfoldsraw.py
@@ -10,55 +10,44 @@ num_classes = int(sys.argv[3])
 
 # set up dataset folds for cross-validation
 epochs_path = 'data/epochs_{0}c/'.format(str(num_classes))
-cv_path = 'data/cv_{0}c/'.format(str(num_classes))
+cv_path = 'data/cv_raw_{0}c/'.format(str(num_classes))
 dataset_folds = [line.rstrip().split(',') for line in open('cv_folds.txt')]
 epoch_template = '{0}_ew{1}.csv'
 epoch_common_labels = [str(eeg_epoch_width_in_s)]
-cv_template = '{0}_{1}_ew{2}_f{3}.csv'
+cv_template = '{0}_{1}_ew{2}_f{3}_{4}.csv'
 
 for fold, dataset_fold in enumerate(dataset_folds):
     cv_common_labels = [eeg_source, str(eeg_epoch_width_in_s), str(fold)]
-    # process training set
-    train_epochs = []
-    train_labels = []
     print('Processing fold ' + str(fold) + '...')
-    for i in range(7):
-        eeg_epochs = []
-        stage_epochs = []
-        print('Processing training set: ' + dataset_fold[i] + '...')
-        input_filename = epoch_template.format(dataset_fold[i] + '_BL5_' +
+    for i in range(len(dataset_folds)):
+        if i < 7:
+            data_purpose = 'train'
+        else:
+            data_purpose = 'test'
+        print('Processing ' + data_purpose + ' set: ' + dataset_fold[i] +
+              '...')
+        data_filename = (epochs_path +
+                         epoch_template.format(dataset_fold[i] + '_BL5_' +
                                                eeg_source,
-                                               *epoch_common_labels)
-        sd.read_data(epochs_path + input_filename, eeg_epochs)
-        input_filename = epoch_template.format(dataset_fold[i] + '_BL5_labels',
-                                               *epoch_common_labels)
-        sd.read_data(epochs_path + input_filename, stage_epochs)
-        train_epochs.extend(eeg_epochs)
-        train_labels.extend(stage_epochs)
-    # process testing set
-    test_epochs = []
-    test_labels = []
-    for i in range(7, 11):
-        eeg_epochs = []
-        stage_epochs = []
-        print('Processing testing set: ' + dataset_fold[i] + '...')
-        input_filename = epoch_template.format(dataset_fold[i] + '_BL5_' +
-                                               eeg_source,
-                                               *epoch_common_labels)
-        sd.read_data(epochs_path + input_filename, eeg_epochs)
-        input_filename = epoch_template.format(dataset_fold[i] + '_BL5_labels',
-                                               *epoch_common_labels)
-        sd.read_data(epochs_path + input_filename, stage_epochs)
-        test_epochs.extend(eeg_epochs)
-        test_labels.extend(stage_epochs)
-
-    # write training and testing features and labels to files
-    print('Writing to files...')
-    output_filename = cv_template.format('train_data', *cv_common_labels)
-    sd.write_data(cv_path + output_filename, train_epochs)
-    output_filename = cv_template.format('train_labels', *cv_common_labels)
-    sd.write_data(cv_path + output_filename, train_labels)
-    output_filename = cv_template.format('test_data', *cv_common_labels)
-    sd.write_data(cv_path + output_filename, test_epochs)
-    output_filename = cv_template.format('test_labels', *cv_common_labels)
-    sd.write_data(cv_path + output_filename, test_labels)
+                                               *epoch_common_labels))
+        labels_filename = (epochs_path +
+                           epoch_template.format(dataset_fold[i] +
+                                                 '_BL5_labels',
+                                                 *epoch_common_labels))
+        datafile = open(data_filename, mode='r', newline='')
+        labelsfile = open(labels_filename, mode='r', newline='')
+        datareader = csv.reader(datafile)
+        labelsreader = csv.reader(labelsfile)
+        # reset file id at the beginning of training or test set
+        if i == 0 or i == 7:
+            id = 0
+        for data, label in zip(datareader, labelsreader):
+            out_filename = cv_path + cv_template.format(data_purpose,
+                                                        *cv_common_labels, id)
+            outfile = open(out_filename, mode='w', newline='')
+            filewriter = csv.writer(outfile)
+            filewriter.writerow(data + label)
+            outfile.close()
+            id += 1
+        labelsfile.close()
+        datafile.close()
