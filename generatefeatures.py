a01e7c4b2c2b78716ec8dce2954e032feb8a276e
Agustinus Sutandi <sutandia@Agustinuss-MacBook-Pro.local>
2019-10-17 15:31:32 -0700

    Initial commit

---
+++ generatefeatures.py
@@ -0,0 +1,49 @@
+import apputil as au
+import concurrent.futures
+import datautil as du
+import featureextraction as fe
+import os
+
+
+"""Create EEG Epochs from EDF Files"""
+# set command-line arguments parser
+parser = au.set_common_arg_parser('Source epoch.')
+parser.add_argument('source', default=None,
+                    choices=['eeg', 'pp1', 'pp2', 'pp3', 'pp4'],
+                    help='source of epochs to be processed')
+parser.add_argument('featgen', default=None,
+                    choices=['pe', 'vg', 'spectral', 'timed', 'wpe', 'siamese',
+                             'siamesers'], help='feature generator')
+args = parser.parse_args()
+
+# set up file location paths
+if args.no_overlap:
+    epochs_path = os.path.join('data', 'epochs_novl_{0}c'.format(
+        str(args.num_classes)))
+else:
+    epochs_path = os.path.join('data', 'epochs_{0}c'.format(
+        str(args.num_classes)))
+if not os.path.isdir(epochs_path):
+    os.makedirs(epochs_path)
+
+
+def process_file(path, file):
+    filename = os.path.join(path, file)
+    groups = du.read_groups_from_hdf5(filename, args.source)
+    for group in groups:
+        fgroup = '{}/{}'.format(args.source, group)
+        tgroup = '{}/{}'.format(args.featgen, group)
+        print('Processing {}: group = {} to group = {}'.format(filename,
+              fgroup, tgroup))
+        epochs = du.read_data_from_hdf5(filename, fgroup)
+        epochs = fe.process(epochs, args.featgen)
+        du.write_data_to_hdf5(filename, tgroup, epochs)
+
+
+# preprocess epochs from all files
+files = [file for file in os.listdir(epochs_path) if
+         '_ew{}.h5'.format(args.eeg_epoch_width_in_s) in file]
+with concurrent.futures.ProcessPoolExecutor(
+        max_workers=args.num_cpus) as executor:
+    for file in files:
+        executor.submit(process_file, epochs_path, file)
