61a1c4bbf3b095947ecefd7863f8376df442771b
Agustinus Sutandi <sutandia@tigger-mbp.local>
2020-04-17 09:32:27 -0700

    Corrected models list passed to write_reports_to_csv to account for all folds.

--- eval_embedding.py
+++ eval_embedding.py
@@ -33,7 +33,7 @@ models_path = 'models'
 #                 'siam11_4c_ew32_25.yaml',
 #                 'siam12_4c_ew32_25.yaml',
 #                 'siam13_4c_ew32_25.yaml']
-config_files = ['siam12_4c_ew32_25.yaml']
+config_files = ['siam9_4c_ew32_25.yaml']
 train_outfile = 'train_4c_ew32_metrics.csv'
 test_outfile = 'test_4c_ew32_metrics.csv'
 
@@ -48,7 +48,7 @@ def predict_labels_based_on_distance(epochs, avg_embeddings):
     return labels
 
 
-def eval_performance(models_path, config_file):
+def eval_performance(models_path, config_file, fold):
     # read EEG epochs and labels used in training Siamese network
     # set up pandas dataframe from pairdata HDF5 file
     model_file = os.path.join(models_path, config_file)
@@ -56,13 +56,14 @@ def eval_performance(models_path, config_file):
         config_params = yaml.safe_load(cfile)
     num_samples = config_params['num_train_samples']
     if config_params['overlap']:
-        pairdata_file = 'pairdata_BL5_ew{}_{}_{}_{}.h5'
+        pairdata_file = 'pairdata_BL5_ew{}_{}_{}_{}_f{}.h5'
     else:
-        pairdata_file = 'pairdata_novl_BL5_ew{}_{}_{}_{}.h5'
+        pairdata_file = 'pairdata_novl_BL5_ew{}_{}_{}_{}_f{}.h5'
     pairdata_file = pairdata_file.format(eeg_epoch_width_in_s,
                                          num_classes,
                                          batch_size,
-                                         num_samples)
+                                         num_samples,
+                                         fold)
     df = pd.read_hdf(pairdata_file, 'pair_index', mode='r')
     df = df[0:num_samples]
     # get unique list of EEG epochs to read from files
@@ -89,7 +90,7 @@ def eval_performance(models_path, config_file):
 
     # generate embeddings using trained models
     eeg_epochs = generate_embeddings(decimate(eeg_epochs, decimate_factor),
-                                     model_file)
+                                     model_file, fold)
 
     # calculate average embeddings for all classes
     avg_embeddings = du.calc_average_features(eeg_epochs, labels, num_classes)
@@ -102,19 +103,19 @@ def eval_performance(models_path, config_file):
                                                   eeg_epoch_width_in_s,
                                                   'eeg',
                                                   None,
-                                                  dataset_folds[0][0:7])
+                                                  dataset_folds[fold][0:7])
     test_epochs, test_labels = du.build_dataset(data_path,
                                                 num_classes,
                                                 eeg_epoch_width_in_s,
                                                 'eeg',
                                                 None,
-                                                dataset_folds[0][7:11])
+                                                dataset_folds[fold][7:11])
 
     # generate embeddings for all EEG epochs using trained models
     train_epochs = generate_embeddings(decimate(train_epochs, decimate_factor),
-                                       model_file)
+                                       model_file, fold)
     test_epochs = generate_embeddings(decimate(test_epochs, decimate_factor),
-                                      model_file)
+                                      model_file, fold)
 
     # set predicted class based on minimum distance to average embeddings
     predict_train_labels = predict_labels_based_on_distance(train_epochs,
@@ -133,30 +134,41 @@ def eval_performance(models_path, config_file):
     return train_report, test_report
 
 
-def write_reports_to_csv(models, reports, outfile):
+def write_reports_to_csv(models, folds, reports, outfile):
     metrics = ['precision', 'recall', 'f1-score', 'support']
     outputs = []
     # form array of header labels and add to outputs
-    header_labels = ['model', 'accuracy']
+    header_labels = ['model', 'fold', 'accuracy']
     for label in target_names:
         for metric in metrics:
             header_labels.append('{}_{}'.format(label, metric))
     outputs.append(header_labels)
     # form array of metric values and add to outputs
-    for model, report in zip(models, reports):
-        metric_values = [model, report['accuracy']]
+    for i in range(len(models)):
+        metric_values = [models[i], folds[i], reports[i]['accuracy']]
         for label in target_names:
             for metric in metrics:
-                metric_values.append(report[label][metric])
+                metric_values.append(reports[i][label][metric])
         outputs.append(metric_values)
     du.write_data(outfile, outputs)
 
 
 train_reports = []
 test_reports = []
+folds = []
+models = []
 for config_file in config_files:
-    train_report, test_report = eval_performance(models_path, config_file)
-    train_reports.append(train_report)
-    test_reports.append(test_report)
-write_reports_to_csv(config_files, train_reports, train_outfile)
-write_reports_to_csv(config_files, test_reports, test_outfile)
+    print('Processing {}'.format(config_file))
+    files = [file for file in os.listdir(models_path) if
+             config_file[:-5] in file and '_best' in file]
+    for file in files:
+        print('Processing {}'.format(file))
+        fold = int(file.split('_')[4])
+        models.append(config_file)
+        folds.append(fold)
+        train_report, test_report = eval_performance(models_path, config_file,
+                                                     fold)
+        train_reports.append(train_report)
+        test_reports.append(test_report)
+write_reports_to_csv(models, folds, train_reports, train_outfile)
+write_reports_to_csv(models, folds, test_reports, test_outfile)
